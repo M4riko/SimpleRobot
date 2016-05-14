d6785783519db801fe18903e4c6c5487974293cb
M4riko <manuel.vieider@gmail.com>
2016-05-14 14:04:17 +1000

    robot_test update
    Changed the test using RoboResults enumuerator

--- robot_test.py
+++ robot_test.py
@@ -1,6 +1,11 @@
+# Tests the functionality of the simple_Robot class
+
+
 import unittest
 
-from simple_Robot import SimpleRobot
+
+
+from simple_Robot import *
 
 class TestRobot(unittest.TestCase):
 
@@ -12,25 +17,25 @@ class TestRobot(unittest.TestCase):
 
     #   test  place() function
     def test_place00(self):
-        self.assertEqual(self._robot.place(0, 0, 0), 1)
+        self.assertEqual(self._robot.place(0, 0, 0), RoboResults.commandAccepted)
 
     def test_place22(self):
-        self.assertEqual(self._robot.place(2, 2, 0), 1)
+        self.assertEqual(self._robot.place(2, 2, 0), RoboResults.commandAccepted)
 
     def test_placeneg11(self):
-        self.assertEqual(self._robot.place(-1, -1, 0), -1)
+        self.assertEqual(self._robot.place(-1, -1, 0), RoboResults.wrongPlacing)
 
     def test_place66(self):
-        self.assertEqual(self._robot.place(6, 6, 0), -1)
+        self.assertEqual(self._robot.place(6, 6, 0), RoboResults.wrongPlacing)
 
     def test_place111(self):
-        self.assertEqual(self._robot.place(1, 1, 1), 1)
+        self.assertEqual(self._robot.place(1, 1, 1), RoboResults.commandAccepted)
 
     def test_place11n1(self):
-        self.assertEqual(self._robot.place(1, 1, -1), -1)
+        self.assertEqual(self._robot.place(1, 1, -1), RoboResults.wrongPlacing)
 
     def test_place11n5(self):
-        self.assertEqual(self._robot.place(1, 1, 4), -1)
+        self.assertEqual(self._robot.place(1, 1, 4), RoboResults.wrongPlacing)
 
 
     # test report() function
@@ -57,44 +62,44 @@ class TestRobot(unittest.TestCase):
     # test basic movement
     def test_moveNorth(self):
         self._robot.place(1, 1, 0)
-        self.assertEqual(self._robot.move(),1 )
+        self.assertEqual(self._robot.move(),RoboResults.commandAccepted )
         self.assertEqual(self._robot.report(), (1, 2, 0))
 
     def test_moveEast(self):
         self._robot.place(1, 1, 1)
-        self.assertEqual(self._robot.move(),1 )
+        self.assertEqual(self._robot.move(),RoboResults.commandAccepted )
         self.assertEqual(self._robot.report(), (2, 1, 1))
 
     def test_moveSouth(self):
         self._robot.place(1, 1, 2)
-        self.assertEqual(self._robot.move(),1 )
+        self.assertEqual(self._robot.move(),RoboResults.commandAccepted )
         self.assertEqual(self._robot.report(), (1, 0 , 2))
 
     def test_moveWest(self):
         self._robot.place(1, 1, 3)
-        self.assertEqual(self._robot.move(),1 )
+        self.assertEqual(self._robot.move(),RoboResults.commandAccepted )
         self.assertEqual(self._robot.report(), (0, 1 , 3))
 
 
     # try to fall off
     def test_moveFallNorth(self):
         self._robot.place(0, 4, 0)
-        self.assertEqual(self._robot.move(), -1 )
+        self.assertEqual(self._robot.move(), RoboResults.borderReached )
         self.assertEqual(self._robot.report(), (0, 4 , 0))
 
     def test_moveFallEast(self):
         self._robot.place(4, 4, 1)
-        self.assertEqual(self._robot.move(), -1 )
+        self.assertEqual(self._robot.move(), RoboResults.borderReached )
         self.assertEqual(self._robot.report(), (4, 4 , 1))
 
     def test_moveFallSouth(self):
         self._robot.place(4, 0, 2)
-        self.assertEqual(self._robot.move(), -1 )
+        self.assertEqual(self._robot.move(), RoboResults.borderReached )
         self.assertEqual(self._robot.report(), (4, 0 , 2))
 
     def test_moveFallWest(self):
         self._robot.place(0, 0, 3)
-        self.assertEqual(self._robot.move(), -1 )
+        self.assertEqual(self._robot.move(), RoboResults.borderReached )
         self.assertEqual(self._robot.report(), (0, 0 , 3))
 
     # test replace
